64d1d496a65babbc7fb3f1b4843a06e00f94dd53
BryanCrom <BryanCrombach@gmail.com>
2026-03-14 16:21:47 +1300

    feat(sitemap.xml): create sitemap for the website to be indexed.

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,17 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import sitemap from "vite-plugin-sitemap";
+
+const routes = ["/", "/contact"];
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    sitemap({
+      hostname: "https://chryslerclubnz.co.nz",
+      dynamicRoutes: routes,
+      exclude: ["/404", "/admin"],
+    }),
+  ],
 });
